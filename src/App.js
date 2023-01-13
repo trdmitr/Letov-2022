6c29fcdfcdef9fc7f0faaa450055a70bf23e4f22
trdmitr <trdmitr@gmail.com>
2023-01-13 14:02:41 +0400

    init papaparse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Papa from "papaparse";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateData = this.updateData.bind(this);
+    this.state = { data: [] };
+
+  }
+  componentDidMount() {
+    Papa.parse( "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5F_b9Zjd0QP9o2T6rT91qvpf8DzNOtvPYIIQfPbUqQlg6jU_mMT2VVCXPv14vVnQQigGeS-_Tz5lr/pub?output=csv",
+      {
+        download: true,
+        header: true,
+        worker: true,
+        skipEmptyLines: true,
+        step: this.updateData,
+        complete: function() {
+          console.log("this.updateData");
+        }
+      }
+    );
+  }
+  // updateData(result) {
+  //   const data = result.data;
+  //   // Here this is available and we can call this.setState (since it's binded in the constructor)
+  //   this.setState({data: data}); // or shorter ES syntax: this.setState({ data });
+  //   console.log(data);
+  // }
+
+  updateData(result) {
+    console.log(result); // displays whole data
+    this.setState({data: result}); // but gets error here
+}
+ 
+  render(props) {
+    // console.log(data);
+    // Your render function
+    return (
+    <div className="App">Data
+   { this.data.map((dat) => (
+      <ul>
+        <li>
+       {dat.name}
+        </li>
+      </ul>
+    )
+    )}
     </div>
-  );
+    
+    
+    
+    )
+
+  }
 }
 
 export default App;
