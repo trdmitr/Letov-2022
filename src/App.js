812bf368e5793ba3ce8e872f73e5eca6f124036d
trdmitr <trdmitr@gmail.com>
2023-01-14 07:56:43 +0400

    add modal

--- src/App.js
+++ src/App.js
@@ -4,55 +4,62 @@ import Papa from "papaparse";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      songs: [],
+      show: false,
+      close: false,
+    }
     this.updateData = this.updateData.bind(this);
-    this.state = { data: [] };
 
   }
+  handleShowDialog = (id) => {
+    this.setState({ ...this.state, selected: id, show: true });
+    console.log("cliked");
+  };
+  handleHideDialog = () => {
+    this.setState({ ...this.state, show: false });
+    console.log("closed");
+  };
+
   componentDidMount() {
-    Papa.parse( "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5F_b9Zjd0QP9o2T6rT91qvpf8DzNOtvPYIIQfPbUqQlg6jU_mMT2VVCXPv14vVnQQigGeS-_Tz5lr/pub?output=csv",
+    Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5F_b9Zjd0QP9o2T6rT91qvpf8DzNOtvPYIIQfPbUqQlg6jU_mMT2VVCXPv14vVnQQigGeS-_Tz5lr/pub?output=csv",
       {
         download: true,
         header: true,
         worker: true,
         skipEmptyLines: true,
-        step: this.updateData,
-        complete: function() {
-          console.log("this.updateData");
-        }
+
+        complete: this.updateData
       }
     );
   }
-  // updateData(result) {
-  //   const data = result.data;
-  //   // Here this is available and we can call this.setState (since it's binded in the constructor)
-  //   this.setState({data: data}); // or shorter ES syntax: this.setState({ data });
-  //   console.log(data);
-  // }
 
-  updateData(result) {
-    console.log(result); // displays whole data
-    this.setState({data: result}); // but gets error here
-}
- 
-  render(props) {
-    // console.log(data);
-    // Your render function
+  updateData = (result) => {
+    console.log(result.data);
+    const data = result.data
+    this.setState({ songs: data });
+
+  }
+
+  render() {
+    if (this.state.songs.length === 0) {
+      return <h1>load...</h1>
+    }
+    console.log(this.state.songs)
     return (
-    <div className="App">Data
-   { this.data.map((dat) => (
-      <ul>
-        <li>
-       {dat.name}
-        </li>
-      </ul>
-    )
-    )}
-    </div>
-    
-    
-    
-    )
+      <div className="App">Data
+        {this.state.songs.map((song) => (
 
+          <li key={song.id} onClick={() => {
+            this.handleShowDialog(song.id);
+            alert(song.name)
+          }}>
+            {song.name} {song.audio_name1}
+          </li>
+        )
+        )}
+      </div>
+    )
   }
 }
 
---
+++ src/Componets/Modal/MyModal.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Modal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+
+class MyModal extends Component {
+
+    state={
+        openModal : false
+    }
+
+    onClickButton = e =>{
+        e.preventDefault()
+        this.setState({openModal : true})
+    }
+
+    onCloseModal = ()=>{
+        this.setState({openModal : false})
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.onClickButton}>Click Me</button>
+                <Modal open={this.state.openModal} onClose={this.onCloseModal}>
+                    <h1>You Did it!</h1>
+                </Modal>   
+            </div>
+        )
+    }  
+}
+
+export default MyModal;
